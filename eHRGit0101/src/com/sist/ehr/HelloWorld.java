dcbd071d70f61a53a4dc983b5efb370f608b5d06
sist <sist@DESKTOP-499LE1F>
2020-04-07 10:10:04 +0900

    init145

--- eHRGit0101/src/com/sist/ehr/HelloWorld.java
+++ eHRGit0101/src/com/sist/ehr/HelloWorld.java
@@ -3,7 +3,9 @@ package com.sist.ehr;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git");
+		System.out.println("Hello Jamie");
+		System.out.println("Hello Suyeon");
+
 
 	}
 
